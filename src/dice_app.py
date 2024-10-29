a3e50abb8ab49e0aad62de79fea123e01bbc5cbe
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-10-29 09:10:03 +0000

    セッション機能をつけてサイコロの出目と合計値を保持するように修正

--- src/dice_app.py
+++ src/dice_app.py
@@ -2,7 +2,10 @@ import random
 import streamlit as st
 import pandas as pd
 
-dices = []
+
+# セッションを使ってdicesの中身が保持されるようにする
+if "dices" not in st.session_state:  # セッションデータの初期化
+    st.session_state.dices = []
 
 st.title("2つのサイコロを振るアプリ")
 
@@ -10,9 +13,9 @@ if st.button("サイコロを振る"):
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     dice_sum = dice1 + dice2
-    dices.append((dice1, dice2, dice_sum))
+    st.session_state.dices.append((dice1, dice2, dice_sum))
     st.write(f"1つ目のサイコロ:{dice1}/2つ目のサイコロ:{dice2}")
 
-df = pd.DataFrame(dices, columns=["1つ目のサイコロ", "2つ目のサイコロ", "合計"])
+df = pd.DataFrame(st.session_state.dices, columns=["1つ目のサイコロ", "2つ目のサイコロ", "合計"])
 st.dataframe(df)
-st.write("試行回数", len(dices))
+st.write("試行回数", len(st.session_state.dices))
