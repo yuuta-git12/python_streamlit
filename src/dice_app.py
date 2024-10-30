abb4581ac336f3517deb52088318c9b641cb8da2
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-10-30 08:38:06 +0000

    複数回のサイコロをまとめて振る機能追加

--- src/dice_app.py
+++ src/dice_app.py
@@ -9,12 +9,28 @@ if "dices" not in st.session_state:  # セッションデータの初期化
 
 st.title("2つのサイコロを振るアプリ")
 
-if st.button("サイコロを振る"):
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1, 6)
-    dice_sum = dice1 + dice2
-    st.session_state.dices.append((dice1, dice2, dice_sum))
-    st.write(f"1つ目のサイコロ:{dice1}/2つ目のサイコロ:{dice2}")
+multiple = st.toggle("複数回振る", False)
+
+if not multiple:
+    if st.button("サイコロを振る"):
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        dice_sum = dice1 + dice2
+        st.session_state.dices.append((dice1, dice2, dice_sum))
+        st.write(f"1つ目のサイコロ:{dice1}/2つ目のサイコロ:{dice2}")
+else:
+    n = st.slider("回数", 1, 1000, 500)
+    if st.button("サイコロを振る"):
+        for _ in range(n):
+            dice1 = random.randint(1, 6)
+            dice2 = random.randint(1, 6)
+            dice_sum = dice1 + dice2
+            st.session_state.dices.append((dice1, dice2, dice_sum))
+        st.write(f"{n}回振りました。")
+
+reset_button = st.button("リセット")
+if st.session_state.dices and reset_button:
+    st.session_state.dices = []
 
 df = pd.DataFrame(st.session_state.dices, columns=["1つ目のサイコロ", "2つ目のサイコロ", "合計"])
 st.dataframe(df)
